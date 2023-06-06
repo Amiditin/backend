b9342111abd15f3eb5d1cf9bae3e997a62325aa8
Dmitry Fomin <dimafomin02@gmail.com>
2023-06-06 16:56:52 +0300

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OrganizationsModule } from './organizations/organizations.module';
-import { OrganizationEntity } from './organizations/entities/organization.entity';
 import { ResearchersModule } from './researchers/researchers.module';
-import { ResearcherEntity } from './researchers/entities/researcher.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PatientsModule } from './patients/patients.module';
-import { PatientEntity } from './patients/entities/patient.entity';
 import { ResultsModule } from './results/results.module';
-import { ResultEntity } from './results/entities/result.entity';
 
 @Module({
   imports: [
